c0278e66b0f9c7cb4074a5ec1dd327d14f233e6b
PandaFeeder <pandafeeder@gmail.com>
2016-05-20 00:32:41 +0800

    update

--- BackEnd/CoalAlley/settings.py
+++ BackEnd/CoalAlley/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/Users/qusr/GitHub/learnDjango/CoalAlley/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 REST_FRAMEWORK = {
