da0e1c96f556b882b476cf07dc6495c0157e815a
PandaFeeder <pandafeeder@gmail.com>
2016-05-28 23:49:08 +0800

    remove semicolon

--- FrontEnd/src/js/services/ArticalStore.js
+++ FrontEnd/src/js/services/ArticalStore.js
@@ -1,15 +1,15 @@
 function ArticalStore() {
     this.incorporate = function(data) {
-        var that = this;
+        var that = this
         data.forEach(function (item, index) {
-            var slug = item['slug'];
-            that[slug] = item;
+            var slug = item['slug']
+            that[slug] = item
         })
-    };
+    }
     this.addIndex = function(data) {
         data.forEach(function (item, index) {
-            item['index'] = index;
-        });
+            item['index'] = index
+        })
     }
 }
 
