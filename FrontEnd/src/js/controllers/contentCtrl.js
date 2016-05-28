d7dc21f9177e11292f12acebba47b6c2add38b5e
PandaFeeder <pandafeeder@gmail.com>
2016-05-28 23:48:50 +0800

    add request latest for root url which replacing cover

--- FrontEnd/src/js/controllers/contentCtrl.js
+++ FrontEnd/src/js/controllers/contentCtrl.js
@@ -3,8 +3,8 @@
 //use file downloaded from prismjs.org instead
 var Prism = require('../prism/prism')
 
-contentCtrl.$inject = ['getArtical', 'ArticalStore', '$scope','$stateParams']
-function contentCtrl(getArtical, ArticalStore, $scope, $stateParams) {
+contentCtrl.$inject = ['getArtical', 'ArticalStore', '$scope','$stateParams', '$http', 'API', '$q']
+function contentCtrl(getArtical, ArticalStore, $scope, $stateParams, $http, API, $q) {
     var slug = $stateParams.slug
     //console.log('content working')
     $scope.retriveArtical = function(slug, ArticalStore, getArtical) {
@@ -31,7 +31,33 @@ function contentCtrl(getArtical, ArticalStore, $scope, $stateParams) {
             $scope.artical = ArticalStore[slug]
         }
     }
-    $scope.retriveArtical(slug, ArticalStore, getArtical)
+    $scope.retriveLatest = function() {
+        if (ArticalStore.latest) {
+            $scope.artical = ArticalStore.latest
+        } else {
+            var deferred = $q.defer()
+            $http.get(API.articalListUrl+"?ordering=latest").then(
+                 function successCallback(response) {
+                     deferred.resolve(response.data)
+                 },
+                 function errorCallBack(response) {
+                     deferred.reject(response.status)
+                 })
+            deferred.promise.then(
+                function successCallback(data) {
+                    $scope.artical = data
+                    ArticalStore.latest = data
+                },
+                function errorCallback(error) {
+                    $scope.error = error
+                })
+        }
+    }
+    if (!slug) {
+        $scope.retriveLatest()
+    } else {
+        $scope.retriveArtical(slug, ArticalStore, getArtical)
+    }
     //use $watch to push its argument function into digest cycle
     $scope.$watch(function () {
         //console.log("WATCHing...")
