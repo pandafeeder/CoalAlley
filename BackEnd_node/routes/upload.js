6e0ef441561d3999aa36d413edc9b7b469bf33d3
PandaFeeder <pandafeeder@gmail.com>
2017-02-19 00:33:59 +0800

    change upload path

--- BackEnd_node/routes/upload.js
+++ BackEnd_node/routes/upload.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(process.cwd(), 'uploads'))
+        cb(null, path.join(process.cwd(), '../FrontEnd_ReactMobx/media'))
     }, 
     filename: (req, file, cb) => {
         cb(null, file.originalname)
